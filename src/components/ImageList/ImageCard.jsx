ef663b8fcd05c1de75757518d9410c1ed4dc1c36
ShrikantaMazumder <shrikantamazumder@gmail.com>
2020-09-14 10:28:47 +0600

    Added style to image list

--- src/components/ImageList/ImageCard.jsx
+++ src/components/ImageList/ImageCard.jsx
@@ -2,21 +2,20 @@ import React, { useRef, useEffect, useState } from 'react';
 
 const ImageCard = ({ image }) => {
     const [spans, setSpans] = useState(0);
-    const imageRef = useRef()
+    const imageRef = useRef();
 
     useEffect(() => {
-        
         imageRef.current.addEventListener('load', setSpan())
     }, []);
     const setSpan = () => {
         const height = imageRef.current.clientHeight;
-        const span = Math.ceil(height / 10);
-        setSpans( {span})
-        console.log(spans);
+        const spans = Math.ceil(height / 10);
+        setSpans(spans);
     }
     return (
-        <div style={{ gridRowEnd: `span ${spans}` }}>
+        <div style={{ gridRow: `auto / span ${spans}` }}>
             <img ref={imageRef} src={image.urls.regular} alt={image.alt_description}/>
+            
         </div>
     );
 }
